f569e6f698061b2150b7ba151626649e66f0f06e
nb <disha07@mail.ru>
2021-06-27 16:29:19 +0300

    Упражнение 2. Создание непрямоугольной формы Windows

--- Lab1.2/Program.cs
+++ Lab1.2/Program.cs
@@ -14,6 +14,7 @@ namespace Lab1._2
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new nForm());

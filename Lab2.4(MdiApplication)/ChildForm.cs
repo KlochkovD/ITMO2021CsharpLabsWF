c5911b541b173a59a1f9930c6ccc48b2e2a5c506
nb <disha07@mail.ru>
2021-07-03 15:55:07 +0300

    Упражнение 4. Использование элемента управления StatusStrip

---
+++ Lab2.4(MdiApplication)/ChildForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1._4_MdiApplication_
+{
+    public partial class ChildForm : Form
+    {
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ChildTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toggleForegroundToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ToggleMenuItem.Checked)
+            {
+                ToggleMenuItem.Checked = false;
+                ChildTextBox.ForeColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                ToggleMenuItem.Checked = true;
+                ChildTextBox.ForeColor = System.Drawing.Color.Blue;
+            }
+        }
+    }
+}
